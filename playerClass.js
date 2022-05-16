8c370c87a3e228801cc087cc4f886c1ea5e03675
axeOfAbrahamLincoln <kormociadam@gmail.com>
2022-05-16 20:37:37 +0200

    added methods to Player class

--- playerClass.js
+++ playerClass.js
@@ -11,6 +11,23 @@ class Player {
 		this.ppg = playerPPG;
 		this.apg = playerAPG;
 	}
+	updateValues(newValue, atribute) {
+		if (atribute == 1) {
+			this.name = newValue;
+		} else if (atribute == 2) {
+			this.team = newValue;
+		} else if (atribute == 3) {
+			this.rpg = newValue;
+		} else if (atribute == 4) {
+			this.ppg = newValue;
+		} else if (atribute == 5) {
+			this.apg = newValue;
+		}
+	}
+	printCard() {
+		console.log(`name: ${this.name}\nteam: ${this.team}`);
+		console.log(`RPG: ${this.rpg} PPG: ${this.ppg} APG: ${this.apg}\n`);
+	}
 }
 
 module.exports = {
